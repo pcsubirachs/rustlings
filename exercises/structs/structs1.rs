7e21092304ec363700de7bffafdc7bf232902b76
npgeorge <17250377+pcsubirachs@users.noreply.github.com>
2022-03-16 16:28:18 -0700

    struct1

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -3,13 +3,15 @@
 
 // I AM NOT DONE
 
+// classic struct takes defined variables and types
 struct ColorClassicStruct {
     // TODO: Something goes here
-    name = "green",
-    hex = "#00FF00",
+    name: String,
+    hex: String,
 }
 
-struct ColorTupleStruct(/* TODO: Something goes here */);
+// tuples don't need variables, just types
+struct ColorTupleStruct(String, String);
 
 #[derive(Debug)]
 struct UnitStruct;
@@ -21,7 +23,10 @@ mod tests {
     #[test]
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
-        // let green =
+        let green = ColorClassicStruct {
+            name: String::from("green"),
+            hex: String::from("#00FF00"),
+        };
 
         assert_eq!(green.name, "green");
         assert_eq!(green.hex, "#00FF00");
@@ -30,7 +35,7 @@ mod tests {
     #[test]
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
-        // let green =
+        let green = ColorTupleStruct(String::from("green"), String::from("#00FF00"));
 
         assert_eq!(green.0, "green");
         assert_eq!(green.1, "#00FF00");
@@ -39,7 +44,7 @@ mod tests {
     #[test]
     fn unit_structs() {
         // TODO: Instantiate a unit struct!
-        // let unit_struct =
+        let unit_struct = UnitStruct;
         let message = format!("{:?}s are fun!", unit_struct);
 
         assert_eq!(message, "UnitStructs are fun!");
